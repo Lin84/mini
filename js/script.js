ba638189043e3c6474cbc1944bae90ac6992c690
Lin84 <lletuan@gmail.com>
2016-04-26 17:34:53 +0200

    technologie section

--- js/script.js
+++ js/script.js
@@ -37,4 +37,61 @@ $(document).ready(function(){
 			scroll=$(document).scrollTop();
 		})
 	})
+
+// ***slider in "TECHNOLOGIE"********************************************************************
+	// var width = $(window).width();	
+	var ww = $(window).width()
+		var a; /* create variable a to automaticly determinate the amount of column in slider=> "slidesPerView:a" */
+		if (ww<784) {a=1;}		
+		if (ww>=784) {a=3;}
+		// if(ww>=1024){a=3;}
+
+	var mySwiper2 = new Swiper ('.technologie-slider', {
+		/* initial slider created by using swiper*/
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+
+		// If we need pagination
+		pagination: '.swiper-pagination',
+
+		// Navigation arrows
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+
+		// And if we need scrollbar
+		// scrollbar: '.swiper-scrollbar',
+
+		 slidesPerView: a,		 
+
+        //  autoplay: 5000,
+        // autoplayDisableOnInteraction: false
+
+        // slidesPerView: 2,
+         spaceBetween: 35
+	})
+
+	$(window).resize(function(){
+		/* if resizing screen will change the amount of column in section categories*/
+		var ww = $(window).width()		
+		if (ww<784) {mySwiper2.params.slidesPerView = 1;}		
+		if (ww>=784) {mySwiper2.params.slidesPerView = 3;}		
+		// if (ww>=1024) {mySwiper2.params.slidesPerView = 3;}
+	})	
+// -------------------------------------------------------------------------------------------------
+	$(".table__navigation_vykon").on("click",function(){
+		$(".table__navigation").removeClass("table__navigation_active");
+		$(".table__navigation_vykon").addClass("table__navigation_active");
+	})
+	$(".table__navigation_bezpecnost").on("click",function(){
+		$(".table__navigation").removeClass("table__navigation_active");
+		$(".table__navigation_bezpecnost").addClass("table__navigation_active");
+		
+	})
+	$(".table__navigation_prostor").on("click",function(){
+		$(".table__navigation").removeClass("table__navigation_active");
+		$(".table__navigation_prostor").addClass("table__navigation_active");
+	})
+
+
 })
