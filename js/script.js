899ec8096b22d3c743fc41086b5349935b5bbf22
Lin84 <lletuan@gmail.com>
2016-04-29 10:08:35 +0200

    change function of technologie and second commit of posedli

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-// ***remove default setting automoving of top after click on button   **********
+// ***remove default setting automoving of top after click on button   *********
 	$(".btn").on("click",function(){
 		event.preventDefault();
 	})
-// --navbar toggle ************************************************************	
+
+// --navbar toggle *************************************************************	
 	$(".navbar-toggle").on("click",function(){
 		$(".navHeaderCollapse").slideToggle();
 	})
 
+// --to fix problem with hiding problem navbar-collapse after ******************
 	$(window).resize(function() {
-		/* to fix problem with hiding problem navbar-collapse after */
 	    if ($(window).width() >784) {
 	        $(".navbar").show();
 	    }
@@ -38,104 +39,147 @@ $(document).ready(function(){
 		})
 	})
 
-// ***slider in "TECHNOLOGIE"********************************************************************
+// ***slider in "TECHNOLOGIE"***************************************************
 	// var width = $(window).width();	
 	var ww = $(window).width()
-		var a; /* create variable a to automaticly determinate the amount of column in slider=> "slidesPerView:a" */
+		var a; 
+		/* create variable a to automaticly determinate the amount of 
+		column in slider=> "slidesPerView:a" */
 		if (ww<784) {a=1;}		
 		if (ww>=784) {a=3;}
 		// if(ww>=1024){a=3;}
 
 	var mySwiper2 = new Swiper ('.technologie-slider', {
-		/* initial slider created by using swiper*/
-		// Optional parameters
 		direction: 'horizontal',
 		loop: true,
-
-		// If we need pagination
 		pagination: '.swiper-pagination',
-
-		// Navigation arrows
 		nextButton: '.swiper-button-next',
 		prevButton: '.swiper-button-prev',
-
-		// And if we need scrollbar
 		// scrollbar: '.swiper-scrollbar',
-
 		 slidesPerView: a,		 
-
         //  autoplay: 5000,
         // autoplayDisableOnInteraction: false
-
         // slidesPerView: 2,
          spaceBetween: 35
 	})
+	/* if resizing screen will change the amount of column in section categories*/
+	// $(window).resize(function(){
+	// 	var ww = $(window).width()		
+	// 	if (ww<784) {mySwiper2.params.slidesPerView = 1;}		
+	// 	if (ww>=784) {mySwiper2.params.slidesPerView = 3;}		
+	// })	
 
-	$(window).resize(function(){
-		/* if resizing screen will change the amount of column in section categories*/
-		var ww = $(window).width()		
-		if (ww<784) {mySwiper2.params.slidesPerView = 1;}		
-		if (ww>=784) {mySwiper2.params.slidesPerView = 3;}		
-		// if (ww>=1024) {mySwiper2.params.slidesPerView = 3;}
-	})	
-// ***table navigation of technologie *********************************************
-	$(".table__navigation_vykon").on("click",function(){
+// ***table navigation of technologie ******************************************
+	$(".table__navigation").on("click",function(){
 		$(".table__navigation").removeClass("table__navigation_active");
-		$(".table__navigation_vykon").addClass("table__navigation_active");
+		$(this).addClass("table__navigation_active");
 	})
-	$(".table__navigation_bezpecnost").on("click",function(){
-		$(".table__navigation").removeClass("table__navigation_active");
-		$(".table__navigation_bezpecnost").addClass("table__navigation_active");
-		
-	})
-	$(".table__navigation_prostor").on("click",function(){
-		$(".table__navigation").removeClass("table__navigation_active");
-		$(".table__navigation_prostor").addClass("table__navigation_active");
-	})
+
+// *** click on navbar will animate to chose menu section **********************
+$(".btn__link_petr").on("click", function(e) {
+    var msg = $(this).attr("href");
+    var menu = $(".header");
+    $("html, body").stop().animate({ scrollTop:0}, 500); 
+});
+
+$(".btn__link_terezie").on("click", function(e) {
+    var msg = $(this).attr("href");
+    var menu = $(".header");
+    var scrolled=$(document).scrollTop();
+    if(scrolled <669){
+	    $("html, body").stop().animate({ scrollTop: $(msg).offset().top }, 500); 
+    }else{
+    	$("html, body").stop().animate({ scrollTop: $(msg).offset().top - 120 }, 500); 
+    }
+});
+
+$(".btn__link_yemi").on("click", function(e) {
+    var msg = $(this).attr("href");
+    var menu = $(".header");
+    var scrolled=$(document).scrollTop();
+    if(scrolled <1038){
+	    $("html, body").stop().animate({ scrollTop: $(msg).offset().top }, 500); 
+    }else{
+    	$("html, body").stop().animate({ scrollTop: $(msg).offset().top - 120 }, 500); 
+    }
+});
+// *** POSEDLI VICE BUTTON *****************************************************
+var vice=false
+	$(".btn_last-posedli").on("click",function(){
+	  	if(!vice){
+		  	$(".posedli-content__vice_second").slideDown();
+		  	$(".btn_last-posedli").text("MÉNĚ");
+		  	vice=true;
+	  	}else{
+		  	$(".posedli-content__vice_second").slideUp();
+		  	$(".btn_last-posedli").text("VÍCE");
+		  	vice=false;}
+  })
 // *** show and hide detail  ************************************************
 	$(function(){
 		var scroll=$(document).scrollTop();
 		var headerHeight=$(".header").height();
 		$(window).scroll(function(){
-			// var scrolled=$(document).scrollTop();
-			// console.log("scroll: "+scroll);
-			// console.log("scrolled: "+scrolled);
-			// if(scrolled>900){
-			// 	$(".posedli-content__detail-container_first").removeClass("show-posedli");
-			// 	$(".posedli-content__detail-container_first").addClass("hide-posedli");
+			var scrolled=$(document).scrollTop();
+			console.log("scroll: "+scroll);
+			console.log("scrolled: "+scrolled);
+			// if(scrolled>600&&scrolled<670){
+			// 	$(".posedli-content__detail-container_second").css("tranform","scale(1.7)");
+				
 			// }
 			// else{
-			// 	$(".posedli-content__detail-container_first").removeClass("hide-posedli");
-			// 	$(".posedli-content__detail-container_first").addClass("show-posedli");
-			// }
-			// if(scrolled>780){
-			// 	$(".posedli-content__detail-container_second").removeClass("hide-posedli");
-			// 	$(".posedli-content__detail-container_second").addClass("show-posedli");
-			// }
-			// else{
-			// 	$(".posedli-content__detail-container_second").removeClass("show-posedli");
-			// 	$(".posedli-content__detail-container_second").addClass("hide-posedli");
+			// 	$(".posedli-content__detail-container_second").removeClass("show");
 			// }
 
-			// if(scrolled>1200){
-			// 	$(".posedli-content__detail-container_third").removeClass("hide-posedli");
-			// 	$(".posedli-content__detail-container_third").addClass("show-posedli");
+			// if(scrolled>1030&&scrolled<1100){
+			// 	$(".posedli-content__detail-container_third").addClass("show");
 			// }
 			// else{
-			// 	$(".posedli-content__detail-container_third").removeClass("show-posedli");
-			// 	$(".posedli-content__detail-container_third").addClass("hide-posedli");
+			// 	$(".posedli-content__detail-container_third").removeClass("show");
 			// }
-
-			scroll=$(document).scrollTop();
+			// scroll=$(document).scrollTop();
 		})
 	})
-// ****************************************************************
-		var controller = new ScrollMagic.Controller();
-		// build scene
-		var scene = new ScrollMagic.Scene({
-			triggerElement: "#trigger1"
-		})
-		.setTween("#animate1", 0.5, {scale: 2.5}) // trigger a TweenMax.to tween
-		.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
-		.addTo(controller);
+  // ********SCROLL MAGIC **********************************************************
+  // var controller= new ScrollMagic.Controller();
+  // var scenel = new ScrollMagic.Scene({
+  // 	triggerElement:"#trigger1",
+  // 	duration:300
+  // }).setTween(".posedli-content__detail-container_second",0.5,{scale:2})
+
+  // .addIndicators({name:"1 - 0 duration"})
+  // .addTo(controller);
+
+  // var controller= new ScrollMagic.Controller();
+  // var scenel = new ScrollMagic.Scene({
+  // 	triggerElement:"#trigger1",
+  // 	duration:500
+  // }).on("enter",function(){
+  // 	$(".posedli-content__detail-container_second").css("transform","scale(1.7)");
+  // 	$(".posedli-content__detail-container_second").css("z-index","99");
+  // }).on("leave",function(){
+  // 	$(".posedli-content__detail-container_second").css("transform","scale(1)");
+  // })
+  // .addIndicators({name:"changing scale"})
+  // .addTo(controller);	
+
+  // var scenel = new ScrollMagic.Scene({
+  // 	triggerElement:"#trigger2",
+  // 	duration:300
+  // }).on("enter",function(){
+  // 	$(".posedli-content__detail-container_third").css("transform","scale(1.7)");
+  // 	$(".posedli-content__detail-container_third").css("z-index","99");
+  // }).on("leave",function(){
+  // 	$(".posedli-content__detail-container_third").css("transform","scale(1)");
+  // })
+  // .addIndicators({name:"changing scale"})
+  // .addTo(controller);	
+  // var scenel = new ScrollMagic.Scene({
+  // 	triggerElement:"#trigger2"
+  // }).setTween(".posedli-content__detail-container_second",0.5,{scale:1})
+  // .addIndicators({name:"1 - 0 duration"})
+  // .addTo(controller);
+
+
+// **************************************************************************
 })
